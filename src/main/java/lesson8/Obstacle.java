a73a76f429d848dfa7282d979f04efbe87638f03
Vladimir Petrov <vl.pva@yandex.ru>
2021-11-17 21:25:33 +0300

    lesson7

--- src/main/java/lesson8/Obstacle.java
+++
@@ -1,4 +0,0 @@
-package lesson8;
-
-public interface Obstacle {
-}
--- src/main/java/lesson8/Participant.java
+++
@@ -1,6 +0,0 @@
-package lesson8;
-
-public interface Participant {
-    boolean run(int length);
-    boolean jump(float height);
-}
